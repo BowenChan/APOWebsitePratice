0531de0480088166f216d1f6c424c622c5ba03e3
BowenChan <BowenmChan@gmail.com>
2014-12-19 14:58:28 -0800

    created header
    linled index to header

---
+++ header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+	<title> Header</title>
+</head>
+<body>
+
+	<table width="100%" height="56" border="1">
+	  <tr>
+	   	<th scope="col"><a href = "index.html">Home</a></th>
+	    <th scope="col"><a href = "forums.html">Forums</a></th>
+	    <th scope="col"><a href = "calendar.html">Calendar</a></th>
+	  	<th scope="col"><a href = "submitform.html">Submit a Service</a></th>
+	    <th scope="col"><a href = "submitforme.html">Submit an event</a></th>
+	    <th scope="col"><a href = "contact.html"> Contact Us</a></th>
+	    <th scope="col"><a href = "login.html">Login</a></th>
+	  </tr>
+	</table>
+
+</body>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<link rel= "stylesheet" type = "text/css" href = "stylesheet/style.css">
+<meta charset="utf-8">
+<title>Home Page</title>
+</head>
+
+<body>
+  <?php include('header.php'); ?>
+  
+</body>
+</html>
